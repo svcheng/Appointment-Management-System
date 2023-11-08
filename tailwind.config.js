c0b9ca5d2898d96ab86dc2d13ade20437851181b
lexilure <alexandrapadua01@gmail.com>
2023-11-08 19:38:16 +0800

    fixed welcome.hbs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,18 @@
 module.exports = {
   content: ["./views/layouts/*.{html,js,hbs}"],
   theme: {
-    extend: {},
-    colors: {
+    extend: {    
+      colors: {
       transparent: 'transparent',
       current: 'currentColor',
       'light-green' : '#DDE8B9',
       'light-yellow': '#E8D2AE',
       'light-orange':'#D7B29D',
       'light-red':'#CB8589',
-      'brown' :'#796465',
-      
-    }
+      'brown': '#796465'
+            }
+  },
+
   },
   plugins: [],
 }
