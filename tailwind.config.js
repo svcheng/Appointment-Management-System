72fc2077b5e5744d7e9c6a36a3f268602304bab8
lexilure <alexandrapadua01@gmail.com>
2023-11-08 16:41:34 +0800

    added custom colors for tailwind and color for admin login text

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./views/layouts/*.{html,js,hbs}"],
   theme: {
-    extend: {}
-
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'light-green' : '#DDE8B9',
+      'light-yellow': '#E8D2AE',
+      'light-orange':'#D7B29D',
+      'light-red':'#CB8589',
+      'brown' :'#796465',
+      
+    }
   },
   plugins: [],
 }
