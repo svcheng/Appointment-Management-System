63aa89008b5277ea52623480ebef2af12b83b123
Rom-Salen <rommel_salen@dlsu.edu.ph>
2023-11-16 06:25:59 +0800

    Update register.js added Email Checker

--- public/js/register.js
+++ public/js/register.js
@@ -2,13 +2,26 @@ document.getElementById("regForm").addEventListener("submit", (e) => {
     e.preventDefault()
 })
 
+let verify = '';
+
+const generateRandomString = (length) => {
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let randomString = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      randomString += charset[randomIndex];
+    }
+    return randomString;
+  }
+
 document.getElementById("regBtn").addEventListener("click", async (e) => {
     const errorMsg = document.getElementById("r_error")
 
     const storeName = document.getElementById("storeName").value
     const password = document.getElementById("password").value
+    const captcha = document.getElementById("captcha").value
 
-    const res = await fetch(`/register/${storeName}/${password}`, {
+    const res = await fetch(`/register/${storeName}/${password}/${captcha}`, {
         method: "POST"
     })
 
@@ -19,5 +32,29 @@ document.getElementById("regBtn").addEventListener("click", async (e) => {
     else {
         // request admin page from server
         window.location.href = '/admin/' + storeName
+    }
+})
+
+document.getElementById("sendCode").addEventListener("click", async (e) =>{
+
+    const receivedEmail = document.getElementById("email").value
+
+    if(document.getElementById("email").checkValidity()){
+    const captcha = generateRandomString(6);
+    verify = captcha;
+    const res = await fetch(`/sendData/${receivedEmail}/${captcha}`, {
+        method: 'POST', 
+    });
+    } else {
+        alert('Please enter proper email');
+    }
+})
+
+document.getElementById("testingCaptcha").addEventListener("click", async (e) =>{
+    const captcha = document.getElementById("captcha").value
+    if(captcha === verify){
+        alert('CORRECT!');
+    } else {
+        alert('WRONG!');
     }
 })
