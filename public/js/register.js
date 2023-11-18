9ffcbcf28fe00877cd7c1bfbdab62971d9ab7f0a
Rom-Salen <rommel_salen@dlsu.edu.ph>
2023-11-18 23:33:16 +0800

    Update register.js

--- public/js/register.js
+++ public/js/register.js
@@ -2,8 +2,10 @@ document.getElementById("regForm").addEventListener("submit", (e) => {
     e.preventDefault()
 })
 
+//Code Verification saved on Client Side
 let verify = '';
 
+//Generates a Random String for Email Verification
 const generateRandomString = (length) => {
     const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     let randomString = '';
@@ -16,45 +18,68 @@ const generateRandomString = (length) => {
 
 document.getElementById("regBtn").addEventListener("click", async (e) => {
     const errorMsg = document.getElementById("r_error")
+    const errorCode = document.getElementById("r_code")
 
     const storeName = document.getElementById("storeName").value
     const password = document.getElementById("password").value
     const codeVerify = document.getElementById("codeVerify").value
+    const receivedEmail = document.getElementById("email").value
 
-    const res = await fetch(`/register/${storeName}/${password}/${codeVerify}`, {
-        method: "POST"
-    })
+    //Confirms if the code matches
+    if(codeVerify === verify){
+        errorCode.hidden = true
+        //Sends data OR Checks if name already exists
+        const res = await fetch(`/register/${storeName}/${password}/${receivedEmail}`, {
+            method: "POST"
+        })
 
-    if (!res.ok) {
-        errorMsg.hidden = false
-        console.log("Incorrect register")
+            if (!res.ok) {
+                errorMsg.hidden = false
+                console.log("Incorrect register")
+            } 
+            else {
+                // request admin page from server
+                window.location.href = '/admin/' + storeName
+            }
+
     } 
     else {
-        // request admin page from server
-        window.location.href = '/admin/' + storeName
+        errorMsg.hidden = true
+        errorCode.hidden = false
+        console.log("Incorrect code")
     }
 })
 
 document.getElementById("sendCode").addEventListener("click", async (e) =>{
 
+    const emailError = document.getElementById("r_email")
     const receivedEmail = document.getElementById("email").value
+    const sendCodeButton = document.getElementById("sendCode");
+   
 
     if(document.getElementById("email").checkValidity()){
-    const codeVerify = generateRandomString(6);
-    verify = codeVerify;
-    const res = await fetch(`/sendData/${receivedEmail}/${codeVerify}`, {
-        method: 'POST', 
-    });
+        const codeVerify = generateRandomString(6);
+        verify = codeVerify;
+        emailError.hidden = true;
+        sendCodeButton.style.opacity = 0.5;
+        sendCodeButton.style.cursor = "auto";
+        sendCodeButton.classList.remove("Enabled");
+        sendCodeButton.classList.add("Disabled");
+        sendCodeButton.disabled = true;
+        const res = await fetch(`/sendData/${receivedEmail}/${codeVerify}`, {
+            method: 'POST', 
+        });
     } else {
-        alert('Please enter proper email');
+        emailError.hidden = false;
     }
 })
 
-document.getElementById("testingMatch").addEventListener("click", async (e) =>{
-    const codeVerify = document.getElementById("codeVerify").value
-    if(codeVerify === verify){
-        alert('CORRECT!');
-    } else {
-        alert('WRONG!');
-    }
-})
+// document.getElementById("testingMatch").addEventListener("click", async (e) =>{
+//     const codeVerify = document.getElementById("codeVerify").value
+//     if(codeVerify === verify){
+//         alert('CORRECT!');
+//     } else {
+//         alert('WRONG!');
+//     }
+// })
+// Testing Button - if trying to immediately check if captcha matching is correct or not
